0d852ead2a1b0bf44a316bc4e19e753607d97fa3
jtyers <40065647+jtyers@users.noreply.github.com>
2023-06-23 22:29:12 +0100

    doc: swagger docs updates

--- web/handlers.go
+++ web/handlers.go
@@ -16,13 +16,14 @@ func NewThreatModelHandlers(ts service.ThreatModelService) *ThreatModelHandlers 
 	return &ThreatModelHandlers{threatModelService: ts}
 }
 
-// @Summary Retrieves a threatModel by ID.
-// @ID get-threat-model-by-id
+// @Summary Retrieves threat models by threat model ID
 // @Produce json
-// @Param threatModelID path string true "threat model ID"
-// @Success 200 {object} m.ThreatModel
-// @Failure 404 {string} string "Not Found"
-// @Router /api/v1/threatmodel/{threatModelID} [get]
+// @Param id path string true "The threat model ID to retrieve data for"
+// @Security firebase
+// @Success 200 {object} m.ThreatModel "The threat model data"
+// @Failure 401 {string} string "If the token supplied is invalid, expired or does not have access to call this API."
+// @Failure 404 {string} string "If the threat model ID does not exist or is not visible to this user."
+// @Router /api/v1/threatmodel/{id} [get]
 func (th *ThreatModelHandlers) GetThreatModelHandler(c *gin.Context) {
 	threatModelIDStr := c.Param("threatModelID")
 	threatModelID := m.NewThreatModelIDP(threatModelIDStr)
@@ -35,10 +36,11 @@ func (th *ThreatModelHandlers) GetThreatModelHandler(c *gin.Context) {
 	}
 }
 
-// @Summary Get all Threat Models
-// @ID get-all-threat-models
+// @Summary Retrieves all threat models visible to the user
 // @Produce json
-// @Success 200 {object} []m.ThreatModel
+// @Security firebase
+// @Success 200 {array} m.ThreatModel "The threat model data"
+// @Failure 401 {string} string "If the token supplied is invalid, expired or does not have access to call this API."
 // @Router /api/v1/threatmodel [get]
 func (th *ThreatModelHandlers) GetThreatModelsHandler(c *gin.Context) {
 	result, err := th.threatModelService.GetAll(c)
@@ -49,12 +51,14 @@ func (th *ThreatModelHandlers) GetThreatModelsHandler(c *gin.Context) {
 	}
 }
 
-// @Summary Create a new Threat Model
-// @ID create-threat-model
+// @Summary Create a new ThreatModel
+// @Accept json
 // @Produce json
-// @Param data body m.ThreatModel true "todo data"
-// @Success 200 {object} m.ThreatModel
-// @Failure 400 {string} string "Nope"
+// @Param data body m.ThreatModelParams true "Parameters for the threat model to create"
+// @Security firebase
+// @Success 200 {object} m.ThreatModel "The created ThreatModel"
+// @Failure 400 {string} string "If the threat model data supplied was invalid or badly formed, or any field failed validation (such as a missing required field or a value out of range), or an invalid ID supplied for any fields that accept IDs"
+// @Failure 401 {string} string "If the token supplied is invalid, expired or does not have access to call this API"
 // @Router /api/v1/threatmodel [put]
 func (th *ThreatModelHandlers) PutThreatModelHandler(c *gin.Context) {
 	var t m.ThreatModelParams
@@ -74,13 +78,16 @@ func (th *ThreatModelHandlers) PutThreatModelHandler(c *gin.Context) {
 	c.PureJSON(http.StatusOK, result)
 }
 
-// @Summary Update a new Threat Model
-// @ID update-threat-model
+// @Summary Update a ThreatModel
+// @Accept json
 // @Produce json
-// @Param data body m.ThreatModel true "todo data"
-// @Success 200 {object} m.ThreatModel
-// @Failure 400 {string} string "Nope"
-// @Router /api/v1/threatmodel/{threatModelID} [patch]
+// @Param id path string true "The threat model ID to update"
+// @Security firebase
+// @Param data body m.ThreatModelParams true "The parameters containing fields to update"
+// @Success 200 {object} m.ThreatModel "The (full) updated threat model data"
+// @Failure 400 {string} string "If the threat model data supplied was invalid or badly formed, or any field failed validation (such as a missing required field or a value out of range), or an invalid ID supplied for any fields that accept IDs"
+// @Failure 401 {string} string "If the token supplied is invalid, expired or does not have access to call this API"
+// @Router /api/v1/threatmodel/{id} [patch]
 func (th *ThreatModelHandlers) PatchThreatModelHandler(c *gin.Context) {
 	threatModelIDStr := c.Param("threatModelID")
 	threatModelID := m.NewThreatModelIDP(threatModelIDStr)
@@ -102,13 +109,14 @@ func (th *ThreatModelHandlers) PatchThreatModelHandler(c *gin.Context) {
 	c.PureJSON(http.StatusOK, t)
 }
 
-// @Summary Delete a threat model
-// @ID delete-threat-model
+// @Summary Delete a ThreatModel by ID
 // @Produce json
-// @Param threatModelID path string true "threat model ID"
-// @Success 200 {object} m.ThreatModel
-// @Failure 404 {string} string "Not Found"
-// @Router /api/v1/threatmodel/{threatModelID} [delete]
+// @Param id path string true "ThreatModel ID"
+// @Security firebase
+// @Success 200 {string} string "Returned when the delete succeeds."
+// @Failure 401 {string} string "If the token supplied is invalid, expired or does not have access to call this API."
+// @Failure 404 {string} string "If the supplied threat model ID does not exist or is not visible to this user."
+// @Router /api/v1/threatmodel/{id} [delete]
 func (th *ThreatModelHandlers) DeleteThreatModelHandler(c *gin.Context) {
 	threatModelIDStr := c.Param("threatModelID")
 	threatModelID := m.NewThreatModelIDP(threatModelIDStr)
